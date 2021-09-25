c3e0de40af4cf9c6dc831831235073a40ec684ad
eddiezh-520 <xiezhen520po@gmail.com>
2021-09-25 15:18:21 +0800

    test commit 2

--- app/src/main/java/com/example/finalresdemo/utils/Constant.java
+++ app/src/main/java/com/example/finalresdemo/utils/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
 
     public static final String LOGIN_SUCCESS = "登陆成功";
     public static final String ACCOUNT_OR_PWD_EMPTY = "账户或密码不能为空";
-    public static final String ACNT_OR_PWD_EMPTY = "账户或密码不能为空";
+    
 
 }
